84491cef4ab2fb06eaf88f31a19e53cf4289c57f
Mino <mino@minomino.org>
2016-10-29 03:01:25 +0200

    setup: Learned distutils sucks and setuptools is good.

--- setup.py
+++ setup.py
@@ -18,26 +18,43 @@
 # You should have received a copy of the GNU General Public License
 # along with mindl. If not, see <http://www.gnu.org/licenses/>.
 
+import pip
+from setuptools import setup, find_packages
+
 import mindl
 
-from distutils.core import setup
+links = []
+requires = []
 
-setup(name="mindl",
-      version=mindl.__version__,
-      description="A plugin-based downloading tool.",
-      author="Mino",
-      author_email="mino@minomino.org",
-      packages=["mindl", "mindl.plugins"],
-      install_requires=["requests", "selenium"],
-      classifiers = [
-              "Programming Language :: Python",
-              "Programming Language :: Python :: 3",
-              "Development Status :: 4 - Beta",
-              "Environment :: Console",
-              "Operating System :: Microsoft :: Windows",
-              "Operating System :: POSIX",
-              "Topic :: Multimedia :: Graphics",
-              "Topic :: Utilities",
-              "Topic :: Internet :: WWW/HTTP",
-              ]
-     )
+requirements = pip.req.parse_requirements("requirements.txt",
+    session=pip.download.PipSession())
+
+for item in requirements:
+    if getattr(item, "url", None):  # Older pip has url
+        links.append(str(item.url))
+    if getattr(item, "link", None):  # newer pip has link
+        links.append(str(item.link))
+    if item.req:
+        requires.append(str(item.req)) # always the package name 
+
+setup(
+    name="mindl",
+    version=mindl.__version__,
+    description="A plugin-based downloading tool.",
+    author="Mino",
+    author_email="mino@minomino.org",
+    packages=find_packages(),
+    install_requires=requires,
+    dependency_links=links,
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Utilities",
+        "Topic :: Internet :: WWW/HTTP",
+    ]
+)
