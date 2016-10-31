b687eb7b924a40c56b73a8b4aa2aa41a1622ee47
Mino <mino@minomino.org>
2016-10-31 04:22:46 +0100

    Added run.py, which can be used to run mindl as a regular script. Useful for pdb.

---
+++ run.py
@@ -0,0 +1,24 @@
+# mindl - A plugin-based downloading tool.
+# Copyright (C) 2016 Mino <mino@minomino.org>
+
+# This file is part of mindl.
+
+# mindl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# mindl is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with mindl. If not, see <http://www.gnu.org/licenses/>.
+
+import runpy
+import sys
+
+if __name__ == "__main__":
+    sys.argv[0] = "mindl"
+    runpy._run_module_as_main("mindl")
